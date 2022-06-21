a91da270debcfeb4878c46402b0df52d134ce2dc
Hayapurchase <Eric.Lesilau@strathmore.edu>
2022-06-22 00:04:13 +0300

    1

--- app/Controllers/Admin/Admin.php
+++ app/Controllers/Admin/Admin.php
@@ -1,7 +1,8 @@
 <?php 
 
 namespace App\Controllers\Admin;
-
+use App\Controllers\BaseController;
+use App\Models\UserModel;
 class Admin extends BaseController
 {
 	public function index()
@@ -9,8 +10,15 @@ class Admin extends BaseController
 		$session=session();
 
     	$data=$_SESSION['Username'];
+
+		$users=new UserModel();
+		
+		$data['users']=json_decode(json_encode($users
+											->whereIn('users.is_deleted',[0])
+											->paginate()),true);
+
         
-     return view('admin',$data);
+     return view('admin');
 	}
 
 }
---
+++ app/Views/templates/dashboard.php
@@ -0,0 +1,19 @@
+<div class='.container-fluid'>
+<img class='img-sm mx-auto d-block' width='200px' height='200px' src="<?php echo base_url('images/NMS.jpg')?>" class='logo'>
+<nav class='navbar navbar-dark bg-primary'>
+<div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Users</a>
+        <a class="nav-link" href="#">Employees</a>
+        <a class="nav-link" href="#">Finance</a>
+        <a class="nav-link disabled">Disabled</a>
+      </div>
+    </div>
+  </div>
+</nav>
+</div>
